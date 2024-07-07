728960b00e1e33e8a351bbbd6fd46ae3802c32ab
aysel.m <aysel.m@vitanur.com>
2024-07-07 23:44:54 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,686 @@
+const data = [
+    {
+        "name": "column 1",
+        "font_weight": "bold",
+        "font_style": "normal",
+        "orientation": "horizontal",
+        "text_align": "center",
+        "font_size": "12",
+        "place": "top",
+        "is_sub": "0",
+        "children": [
+            {
+                "name": "column 1-1",
+                "font_weight": "normal",
+                "font_style": "normal",
+                "orientation": "horizontal",
+                "text_align": "center",
+                "font_size": "12",
+                "place": null,
+                "is_sub": "0",
+                "children": [
+                    {
+                        "name": "column 1-1-1",
+                        "font_weight": "normal",
+                        "font_style": "normal",
+                        "orientation": "horizontal",
+                        "text_align": "center",
+                        "font_size": "12",
+                        "place": null,
+                        "is_sub": "0",
+                        "children": [
+                            {
+                                "name": "column 1-1-1-1",
+                                "font_weight": "normal",
+                                "font_style": "normal",
+                                "orientation": "horizontal",
+                                "text_align": "center",
+                                "font_size": 12,
+                                "is_sub": "0",
+                                "children": []
+                            }
+                        ],
+                        "id": 19
+                    },
+                    {
+                        "name": "column 1-1-2",
+                        "font_weight": "normal",
+                        "font_style": "normal",
+                        "orientation": "horizontal",
+                        "text_align": "center",
+                        "font_size": "12",
+                        "place": null,
+                        "is_sub": "0",
+                        "children": [],
+                        "id": 20
+                    }
+                ],
+                "id": 5
+            },
+            {
+                "name": "column 1-2",
+                "font_weight": "normal",
+                "font_style": "normal",
+                "orientation": "horizontal",
+                "text_align": "center",
+                "font_size": "12",
+                "place": null,
+                "is_sub": "0",
+                "children": [
+                    {
+                        "name": "column 1-2-1",
+                        "font_weight": "normal",
+                        "font_style": "normal",
+                        "orientation": "horizontal",
+                        "text_align": "center",
+                        "font_size": "12",
+                        "place": null,
+                        "is_sub": "0",
+                        "children": [],
+                        "id": 21
+                    },
+                    {
+                        "name": "column 1-2-2",
+                        "font_weight": "normal",
+                        "font_style": "normal",
+                        "orientation": "horizontal",
+                        "text_align": "center",
+                        "font_size": "12",
+                        "place": null,
+                        "is_sub": "0",
+                        "children": [],
+                        "id": 22
+                    }
+                ],
+                "id": 6
+            }
+        ],
+        "id": 1
+    },
+    {
+        "name": "column 2",
+        "font_weight": "bold",
+        "font_style": "normal",
+        "orientation": "horizontal",
+        "text_align": "center",
+        "font_size": "12",
+        "place": "top",
+        "is_sub": "0",
+        "children": [],
+        "id": 2
+    },
+    {
+        "name": "column 3",
+        "font_weight": "bold",
+        "font_style": "normal",
+        "orientation": "vertical",
+        "text_align": "center",
+        "font_size": "12",
+        "place": "top",
+        "is_sub": "0",
+        "children": [
+            {
+                "name": "column 3-1",
+                "font_weight": "normal",
+                "font_style": "normal",
+                "orientation": "vertical",
+                "text_align": "center",
+                "font_size": "12",
+                "place": null,
+                "is_sub": "1",
+                "children": [],
+                "id": 24
+            },
+            {
+                "name": "column 3-2",
+                "font_weight": "normal",
+                "font_style": "normal",
+                "orientation": "vertical",
+                "text_align": "center",
+                "font_size": "12",
+                "place": null,
+                "is_sub": "1",
+                "children": [],
+                "id": 25
+            },
+            {
+                "name": "column 3-3",
+                "font_weight": "normal",
+                "font_style": "normal",
+                "orientation": "vertical",
+                "text_align": "center",
+                "font_size": "12",
+                "place": null,
+                "is_sub": "0",
+                "children": [],
+                "id": 26
+            }
+        ],
+        "id": 23
+    },
+    {
+        "name": "column 4",
+        "font_weight": "normal",
+        "font_style": "normal",
+        "orientation": "horizontal",
+        "text_align": "center",
+        "font_size": 12,
+        "place": "top",
+        "is_sub": "0",
+        "children": [
+            {
+                "name": "column 4-1",
+                "font_weight": "normal",
+                "font_style": "normal",
+                "orientation": "horizontal",
+                "text_align": "center",
+                "font_size": 12,
+                "is_sub": "0",
+                "children": []
+            }
+        ]
+    },
+    {
+        "name": "row 1",
+        "font_weight": "normal",
+        "font_style": "normal",
+        "orientation": "horizontal",
+        "text_align": "left",
+        "font_size": "12",
+        "place": "left",
+        "is_sub": "0",
+        "children": [
+            {
+                "name": "row 1-1",
+                "font_weight": "normal",
+                "font_style": "normal",
+                "orientation": "horizontal",
+                "text_align": "center",
+                "font_size": "12",
+                "place": null,
+                "is_sub": "0",
+                "children": [],
+                "id": 7
+            },
+            {
+                "name": "row 1-2",
+                "font_weight": "normal",
+                "font_style": "normal",
+                "orientation": "horizontal",
+                "text_align": "center",
+                "font_size": "12",
+                "place": null,
+                "is_sub": "0",
+                "children": [],
+                "id": 8
+            }
+        ],
+        "id": 3
+    },
+    {
+        "name": "row 2",
+        "font_weight": "normal",
+        "font_style": "normal",
+        "orientation": "horizontal",
+        "text_align": "left",
+        "font_size": "12",
+        "place": "left",
+        "is_sub": "0",
+        "children": [],
+        "id": 4
+    },
+    {
+        "name": "row 3",
+        "font_weight": "bold",
+        "font_style": "normal",
+        "orientation": "vertical",
+        "text_align": "center",
+        "font_size": "12",
+        "place": "left",
+        "is_sub": "0",
+        "children": [
+            {
+                "name": "row 3-1",
+                "font_weight": "normal",
+                "font_style": "normal",
+                "orientation": "horizontal",
+                "text_align": "center",
+                "font_size": "12",
+                "place": null,
+                "is_sub": "1",
+                "children": [],
+                "id": 29
+            },
+            {
+                "name": "row 3-1-2",
+                "font_weight": "normal",
+                "font_style": "normal",
+                "orientation": "horizontal",
+                "text_align": "center",
+                "font_size": "12",
+                "place": null,
+                "is_sub": "1",
+                "children": [],
+                "id": 30
+            },
+            {
+                "name": "row 3-1-3",
+                "font_weight": "normal",
+                "font_style": "normal",
+                "orientation": "horizontal",
+                "text_align": "center",
+                "font_size": "12",
+                "place": null,
+                "is_sub": "1",
+                "children": [],
+                "id": 31
+            }
+        ],
+        "id": 28
+    },
+    {
+        "name": "row 4",
+        "font_weight": "normal",
+        "font_style": "normal",
+        "orientation": "horizontal",
+        "text_align": "left",
+        "font_size": 12,
+        "place": "left",
+        "is_sub": "0",
+        "children": [
+            {
+                "name": "row 4-1",
+                "font_weight": "normal",
+                "font_style": "normal",
+                "orientation": "horizontal",
+                "text_align": "center",
+                "font_size": 12,
+                "is_sub": "0",
+                "children": []
+            }
+        ]
+    }
+];
+const jsonData =[
+    {
+    "name": "column 1",
+    "font_weight": "bold",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": "12",
+    "place": "top",
+    "is_sub": "0",
+    "children": [
+    {
+    "name": "column 1-1",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": "12",
+    "place": null,
+    "is_sub": "0",
+    "children": [
+    {
+    "name": "column 1-1-1",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": "12",
+    "place": null,
+    "is_sub": "0",
+    "children": [
+    {
+    "name": "column 1-1-1-1",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": 12,
+    "is_sub": "0",
+    "children": []
+    }
+    ],
+    "id": 19
+    },
+    {
+    "name": "column 1-1-2",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": "12",
+    "place": null,
+    "is_sub": "0",
+    "children": [],
+    "id": 20
+    }
+    ],
+    "id": 5
+    },
+    {
+    "name": "column 1-2",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": "12",
+    "place": null,
+    "is_sub": "0",
+    "children": [
+    {
+    "name": "column 1-2-1",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": "12",
+    "place": null,
+    "is_sub": "0",
+    "children": [],
+    "id": 21
+    },
+    {
+    "name": "column 1-2-2",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": "12",
+    "place": null,
+    "is_sub": "0",
+    "children": [],
+    "id": 22
+    }
+    ],
+    "id": 6
+    }
+    ],
+    "id": 1
+    },
+    {
+    "name": "column 2",
+    "font_weight": "bold",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": "12",
+    "place": "top",
+    "is_sub": "0",
+    "children": [],
+    "id": 2
+    },
+    {
+    "name": "column 3",
+    "font_weight": "bold",
+    "font_style": "normal",
+    "orientation": "vertical",
+    "text_align": "center",
+    "font_size": "12",
+    "place": "top",
+    "is_sub": "0",
+    "children": [
+    {
+    "name": "column 3-1",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "vertical",
+    "text_align": "center",
+    "font_size": "12",
+    "place": null,
+    "is_sub": "1",
+    "children": [],
+    "id": 24
+    },
+    {
+    "name": "column 3-2",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "vertical",
+    "text_align": "center",
+    "font_size": "12",
+    "place": null,
+    "is_sub": "1",
+    "children": [],
+    "id": 25
+    },
+    {
+    "name": "column 3-3",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "vertical",
+    "text_align": "center",
+    "font_size": "12",
+    "place": null,
+    "is_sub": "0",
+    "children": [],
+    "id": 26
+    }
+    ],
+    "id": 23
+    },
+    {
+    "name": "column 4",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": 12,
+    "place": "top",
+    "is_sub": "0",
+    "children": [
+    {
+    "name": "column 4-1",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": 12,
+    "is_sub": "0",
+    "children": []
+    }
+    ]
+    },
+    {
+    "name": "row 1",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "left",
+    "font_size": "12",
+    "place": "left",
+    "is_sub": "0",
+    "children": [
+    {
+    "name": "row 1-1",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": "12",
+    "place": null,
+    "is_sub": "0",
+    "children": [],
+    "id": 7
+    },
+    {
+    "name": "row 1-2",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": "12",
+    "place": null,
+    "is_sub": "0",
+    "children": [],
+    "id": 8
+    }
+    ],
+    "id": 3
+    },
+    {
+    "name": "row 2",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "left",
+    "font_size": "12",
+    "place": "left",
+    "is_sub": "0",
+    "children": [],
+    "id": 4
+    },
+    {
+    "name": "row 3",
+    "font_weight": "bold",
+    "font_style": "normal",
+    "orientation": "vertical",
+    "text_align": "center",
+    "font_size": "12",
+    "place": "left",
+    "is_sub": "0",
+    "children": [
+    {
+    "name": "row 3-1",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": "12",
+    "place": null,
+    "is_sub": "0",
+    "children": [
+    {
+    "name": "row 3-1-1",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": "12",
+    "place": null,
+    "is_sub": "1",
+    "children": [],
+    "id": 29
+    },
+    {
+    "name": "row 3-1-2",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": "12",
+    "place": null,
+    "is_sub": "1",
+    "children": [],
+    "id": 30
+    },
+    {
+    "name": "row 3-1-3",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": "12",
+    "place": null,
+    "is_sub": "1",
+    "children": [],
+    "id": 31
+    }
+    ],
+    "id": 28
+    },
+    {
+    "name": "row 3-2",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": "12",
+    "place": null,
+    "is_sub": "0",
+    "children": [],
+    "id": 32
+    },
+    {
+    "name": "row 3-3",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": "12",
+    "place": null,
+    "is_sub": "0",
+    "children": [],
+    "id": 33
+    }
+    ],
+    "id": 27
+    },
+    {
+    "name": "row 4",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "left",
+    "font_size": 12,
+    "place": "left",
+    "is_sub": "0",
+    "children": [
+    {
+    "name": "row 4-1",
+    "font_weight": "normal",
+    "font_style": "normal",
+    "orientation": "horizontal",
+    "text_align": "center",
+    "font_size": 12,
+    "is_sub": "0",
+    "children": []
+    }
+    ]
+    }
+    ]
+
+
+function createTable(data) {
+    const tableContainer = document.getElementById('table-container');
+    const table = document.createElement('table');
+    const tbody = document.createElement('tbody');
+
+    function createRow(element, level = 0) {
+        const row = document.createElement('tr');
+        const cell = document.createElement('td');
+        
+        cell.innerText = element.name;
+        cell.style.fontWeight = element.font_weight;
+        cell.style.fontStyle = element.font_style;
+        cell.style.textAlign = element.text_align;
+        cell.style.fontSize = `${element.font_size}px`;
+        
+        if (element.place === 'top') {
+            const th = document.createElement('th');
+            th.colSpan = 2;
+            th.innerText = element.name;
+            th.style.fontWeight = element.font_weight;
+            th.style.fontStyle = element.font_style;
+            th.style.textAlign = element.text_align;
+            th.style.fontSize = `${element.font_size}px`;
+            row.appendChild(th);
+        } else {
+            row.appendChild(cell);
+        }
+
+        tbody.appendChild(row);
+
+        if (element.children && element.children.length > 0) {
+            element.children.forEach(child => createRow(child, level + 1));
+        }
+    }
+
+    data.forEach(element => createRow(element));
+    table.appendChild(tbody);
+    tableContainer.appendChild(table);
+}
+
+createTable(data);
